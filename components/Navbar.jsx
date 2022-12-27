7ae82c5be1f370b5d576c4fe0b13ea3139a767c2
Bayramsch <bayramkmz7240@gmail.com>
2022-12-27 18:26:14 +0100

    cart component has done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,14 +6,15 @@ import { Cart } from './'
 import { useStateContext } from '../context/StateContext'
 
 const Navbar = () => {
-  const {showCart, SetShowCart, totalQuantities } = useStateContext();
+  const {showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className='navbar-container'>
       <p className='logo'>
         <Link href="/">JSM Headphones</Link>
       </p>
 
-      <button type='button' className='cart-icon' onClick={()=> SetShowCart(true)}>
+      <button type="button" className='cart-icon' onClick={()=>setShowCart(true)}>
         <AiOutlineShopping />
         <span className="cart-item-qty">{totalQuantities}</span>
       </button>
